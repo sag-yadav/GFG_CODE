46ff5c0ec6db5f6fb7eaf3180c867d9520bf0511
SAGAR YADAV <sagaryadav15052005@gmail.com>
2026-05-11 22:49:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/While Loop/while-loop.java
+++ Difficulty: Basic/While Loop/while-loop.java
@@ -1,11 +1,5 @@
-import java.util.*;
-
-public class Solution {
-    public static void solve() {
-        Scanner sc = new Scanner(System.in);
-        
-        int x = sc.nextInt();   // read input
-        
+class Solution {
+    public static void utility(int x) {
         while (x >= 0) {
             System.out.print(x + " ");
             x--;
